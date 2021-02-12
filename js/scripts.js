81ca4791a404f6da29d048ba46715119560938d8
tpo-apatheal <77015964+tpo-apatheal@users.noreply.github.com>
2021-02-12 11:06:24 -0800

    change

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,27 @@ let modalWindow;
 const body = document.querySelector('body');
 const gallery = document.querySelector('.gallery');
 const searchContainer = document.querySelector('search-container');
-const userUrl = 'https://randomuser.me/api/?results=12';
-let generateUserProfile;
+const userUrl = 'https://randomuser.me/api/?results=12&nat=us';
+//let generateUserProfile;
 //console.log();
 
 
 
 
 //Fetch request that will request the random users for the page. 
-    fetch (userUrl)
-    //.then(checkStatus)
-    .then((res) => res.json()) //json string
+fetch (userUrl)
+  .then((res) => res.json()) //json string
     //.then(data => console.log(data.results))
-    .then(data => userInfo(data.results))
+  .then(data => {
+    userInfo(data.results);
+    clickCard(data.results);
+  });
 
-
-    function userInfo(parseData) {
-        console.log(parseData);
-        const generateUserProfile = parseData.map((user) => { 
-        return `<div class="card">
+//const users = [];
+function userInfo(parseData) {
+        //console.log(parseData);
+   const generateUserProfile = parseData.map((user) => { 
+      return `<div class="card">
         <div class="card-img-container">
             <img class="card-img" src="${user.picture.large}" alt="profile picture">
         </div>
@@ -36,12 +38,40 @@ let generateUserProfile;
         </div>
     </div>`;
         });
-        gallery.insertAdjacentHTML('beforeend', generateUserProfile);
+      gallery.insertAdjacentHTML('beforeend', generateUserProfile);
        // console.log(generateUserProfile)
     }
 
+  
+    function createModal(user){
+      //console.log(user);
+      const modal = `
+        <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${user.picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="modal-text">${user.email}</p>
+                <p class="modal-text cap">${user.location.city}</p>
+                <hr>
+                <p class="modal-text">${user.phone}</p>
+                <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
+                <p class="modal-text">Birthday: ${user.dob.date}</p>
+            </div>
+        </div>`;
+      gallery.insertAdjacentHTML('beforeend', modal);
+    }
 
-
+    //Event listeners
+    function clickCard(employeeInfo){
+      let userCards = document.querySelectorAll('.card');
+      userCards.forEach(card => {
+        card.addEventListener('click',(e) => {
+            createModal(employeeInfo[4]);
+        });
+      });
+    }
 
 
     //Helper functions
