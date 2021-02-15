de702e4635dc13602220b7bdc56ee27310ec5d6d
tpo-apatheal <77015964+tpo-apatheal@users.noreply.github.com>
2021-02-15 10:16:13 -0800

    change
    updated background color, as well as updated formatters, userInfo function, and comments.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,26 @@
 /*Getting started. Will first need to fetch an image, first and las name, email, and city/location from the API. Then will need to add functionality to 
 the webpage by creating a modal window which displays an image, name, email, city and location, cell number, detailed address, and birthday. Will also need 
-ensure that the modal window can be closed. Will need to utilize regex to correctly format the user's cell number when the information is fetched. */
+ensure that the modal window can be closed. Will need to utilize regex to correctly format the user's cell number and DOB when the information is fetched. */
 
-
-//let modalWindow;
-const body = document.querySelector('body');
+//List of global variables.
 const gallery = document.querySelector('.gallery');
-const searchContainer = document.querySelector('search-container');
 const userUrl = 'https://randomuser.me/api/?results=12&nat=us';
 let index = 0
 let allCards = [];
-//let generateUserProfile;
 //console.log();
 
 
-//Fetch request that will request the random users for the page. 
+/*Fetch request that will request 12 random employees to display on the page. The data is passed to the userInfo and clickCard functions. I declared the
+ url as a variable that I could pass through to the fetch request. */
 fetch (userUrl)
   .then((res) => res.json()) //json string
   .then(data => {
     userInfo(data.results);
     clickCard(data.results);
   });
-  //.then(() => closeModalWindow());
-
+ 
+/*The userInfo function loops through the employees and creates an individual card for each employee and appends it to the html. This function is called 
+in the fetch request where the data is passed through.*/
 function userInfo(parseData) {
         //console.log(parseData);
     parseData.forEach((user) => { 
@@ -41,10 +39,14 @@ function userInfo(parseData) {
        // console.log(generateUserProfile)
     }
 
-  
+  /* The createModal function creates the modal window once a user clicks on an employee. Utilized a template literal for the modal format. Once the
+  createModal function is called below in the clickCard function, the modal is appended to the html. I also declared two variables: employeePhone and 
+  employeeDOB. These variables are assigned the values of the format funcitons below for the telephone and DOB. These variables replace the respective 
+  positions within the template literal. */
     function createModal(user){
       //console.log(user);
       let employeePhone = formatTelephoneNumber(`${user.phone}`);
+      let employeeDOB = formatBirthDate(`${user.dob.date}`);
       const modal = `
         <div class="modal-container">
         <div class="modal">
@@ -57,14 +59,20 @@ function userInfo(parseData) {
                 <hr>
                 <p class="modal-text">${employeePhone}</p>
                 <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
-                <p class="modal-text">Birthday: ${user.dob.date}</p>
+                <p class="modal-text">Birthday: ${employeeDOB}</p>
             </div>
         </div>`;
       gallery.insertAdjacentHTML('beforeend', modal);
     }
        
 
-    //Event listeners
+    /*The two functions below have the event listeners which open and close the modal windows. Function clickCard listens for when the user clicks on 
+    an employee's card and then displays the appropriate modal window. In order to access each individual card, I looped through the employee cards
+    and pushed them into an empty array which which is called "allCards." This made it so each card could be assigned a new index value that could be passed 
+    when calling the createModal function. This ensures that the modal window displays the information for the employee that was clicked. Lastly, the 
+    closeModalWindow function adds functionality to the close button that is within the div that is assigned a modal class. Once the user clicks
+    the close button, the modal container is removed which will allow for a new one to be created when the user selects a different employee. The 
+    closeModalWindow function is called in the clickCard function  along wtih the createModal function.*/
     function clickCard(employeeInfo){
       let userCards = document.querySelectorAll('.card');
       for(let i = 0; i< userCards.length; i++){
@@ -79,35 +87,41 @@ function userInfo(parseData) {
       });
     }
 
-      function closeModalWindow(){
-      const container = document.querySelector(".modal-container");
-      const closeButton = document.getElementById('modal-close-btn');
-       closeButton.addEventListener('click', (e) => {
-          container.remove();
+    function closeModalWindow(){
+    const container = document.querySelector(".modal-container");
+    const closeButton = document.getElementById('modal-close-btn');
+     closeButton.addEventListener('click', (e) => {
+        container.remove();
           //console.log('test');
-        });
-     }
+      });
+    }
 
-
-      //Formatters 
-      const formatTelephoneNumber = (phoneNumber) => {
-        const phone = /^(\d{1,3})(\d{3})(\d{4})$/;
-        return phoneNumber.replace(phone, '$(1) $2-$3');
+    /* The formatters below format the telephone number and date of birth of each employee after their information is received from the 
+    open-source API. The formatTelephone variable includes a function which takes the regex format of the original phone number. I utilized the 
+    slice method to extract the first 14 characters of the phone number. I then globally matched characters that weren't digits and replaced
+    them with an empty string. I used the replace method again to replace the phone number with the correct format (XXX) XXX-XXXX. The formatBirthDate works
+    similar. The employee date of birth included additional information that was attached at the end of the employee birth dates. I utilized the slice 
+    method again which gave the ability to extract the information that was needed which in this case  was the first ten characters. 
+    I then globally matched characters that weren't digits and replaced them with an empty string. This was assigned to a  variable declared as dobSliced. 
+    The format was then replaced again with the correct format mm/dd/yyyy. Both of these variables are called in the createModal function above. */
+    const formatTelephoneNumber = (phoneNumber) => {
+      const phone = /^(\d{3})(\d{3})(\d{4})$/;
+      const phoneSliced = phoneNumber.slice(0,14).replace(/\D/g, '');
+      const correctedFormat = phoneSliced.replace(phone, '($1) $2-$3');
+      return correctedFormat;
       }
 
-    
+    const formatBirthDate = (dob) => {
+      const currentFormat = /^(\d{4})(\d{2})(\d{2})$/;
+      const dobSliced = dob.slice(0,10).replace(/\D/g, '');
+      const correctedFormat = dobSliced.replace(currentFormat, '$2/$3/$1');
+      return correctedFormat;
+    }
 
 
 
+  
 
-
-    //Helper functions
-    /*function checkStatus(response) {
-        if(response.ok){
-          return Promise.resolve(response);
-        } else {
-          return Promise.reject(new Error(response.statusText))
-        }
-      }*/
+   
 
      
